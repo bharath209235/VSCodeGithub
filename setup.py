279ce9bfb5cfc09c845fd2018fe9687ecd5af7f5
bharath209235 <kumarbharath186@gmail.com>
2025-02-16 09:50:32 +0530

    structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='VSCodeGithub',
+version='0.0.1',
+author='bharath209235',
+author_email='kumarbharath186@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
